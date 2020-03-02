a3e66a62ccf73a5bac7ec1004aa55f2e33941e01
Aaron Southammavong <aaroneight07@gmail.com>
2020-03-01 18:15:55 -0800

    removed files

--- config/postgresConfig.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  password: '',
-  ip: 'localhost:5432',
-  database: 'pixie'
-};
-
-module.exports = config;
--- config/spotifyConfig.js
+++
@@ -1,5 +0,0 @@
-
-const tokens = {
-  spotifyToken: 'BQAJ3hxkyNuqf02LfDKYmIpl4_XWtKnItnb5GKbmPaR1SD7VKAYq99iXu7OiJUDNSB2UJ6rWtYv2gOiomVODveDkzAg1_NYQR6P--TSEiv5_VLxsgrO1PLbeTLFAL62XYznOIqUA0e1drnutwArz-5jvEIex'
-}
-module.exports = tokens
